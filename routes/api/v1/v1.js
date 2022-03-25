b220cd4f415543e56a8e8c2c307461a04b192473
nicollh9 <nicollarita06@gmail.com>
2022-03-24 23:17:32 -0600

    Routes update

--- routes/api/v1/v1.js
+++ routes/api/v1/v1.js
@@ -6,6 +6,8 @@ const { passport, jwtMiddleware } = require('./seguridad/jwtHelper');
 const seguridadRoutes = require('./seguridad/seguridad');
 const clientesRoutes = require('./clientes/clientes');
 const empleadosRoutes = require('./empleados/empleados');
+const librosRoutes = require('./libros/libros');
+const fichasRoutes = require('./fichas/fichas');
 router.use(passport.initialize());
 
 //public
@@ -25,6 +27,18 @@ router.use(
     '/empleados',
     verifyApiHeaderToken,
     empleadosRoutes
+);
+
+router.use(
+    '/libros',
+    verifyApiHeaderToken,
+    librosRoutes
+);
+
+router.use(
+    '/fichas',
+    verifyApiHeaderToken,
+    fichasRoutes
 );
 
 module.exports = router;
